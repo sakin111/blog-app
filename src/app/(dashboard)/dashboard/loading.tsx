561b166a902511750deceffea02245aa445833e5
sakin111 <maliksakin53@gmail.com>
2025-10-23 14:05:53 +0600

    .

---
+++ src/app/(dashboard)/dashboard/loading.tsx
@@ -0,0 +1,12 @@
+import Loading from "@/components/ui/Loading";
+
+
+const DashLoading = () => {
+  return (
+    <div className="py-24">
+      <Loading />
+    </div>
+  );
+};
+
+export default DashLoading;
