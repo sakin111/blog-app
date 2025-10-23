42a437ff2a3d95a88c96ac9d32c2a8a77b638477
sakin111 <maliksakin53@gmail.com>
2025-10-23 13:35:07 +0600

    last commit

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,23 +1,11 @@
-"use client";
-
-import { useAuth } from "@/provider/AuthProvider";
-import { useRouter } from "next/navigation";
+import DashboardHome from "@/components/shared/DashboardHome";
 
 export default function DashboardPage() {
-  const { user, loading } = useAuth();
-  const router = useRouter();
 
-  if (loading) return <p>Loading...</p>;
-
-  if (!user) {
-    router.push("/login");
-    return null;
-  }
 
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-semibold">Welcome, {user.name}</h1>
-      <p>Role: {user.role}</p>
+    <div >
+   <DashboardHome/>
     </div>
   );
 }
---
+++ src/components/shared/DashboardHome.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useAuth } from "@/provider/AuthProvider";
+import { useRouter } from "next/navigation";
+
+export default function DashboardHome() {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  if (loading) return <p>Loading...</p>;
+
+  if (!user) {
+    router.push("/login");
+    return null;
+  }
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-semibold">Welcome, {user.name}</h1>
+      <p>Role: {user.role}</p>
+    </div>
+  );
+}
